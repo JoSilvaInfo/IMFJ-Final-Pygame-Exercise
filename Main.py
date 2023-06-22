e655037258008e5dd67ad93ef5a8bf274b2a45a1
JoSilvaInfo <joanafsilva.info@gmail.com>
2023-06-22 19:20:39 +0100

    Add player; Add Boyance base

--- Main.py
+++ Main.py
@@ -1,27 +1,51 @@
 import pygame
-import time
+import math
 import random
 
-# Define the size/resolution of our window
+# Defining screen/window:
+## Define the size/resolution of our window
 res_x, res_y = 1400, 1000
-# Create a window and a display surface
+## Create a window and a display surface
 screen = pygame.display.set_mode((res_x, res_y))
-# Set the pygame window name
+## Set the pygame window name
 pygame.display.set_caption("Pirate Escape")
-# Load background image
-BG = pygame.image.load("img/BG1.png")
-# Re-size background image
-screenUpdate = pygame.transform.scale(BG, (res_x, res_y))
+## Load background image and re-size it 
+BGImg = pygame.transform.scale(pygame.image.load("img/BG1.png"), (res_x, res_y))
 
+
+#Player parameters:
+## Define the size of the player
+pl_with, pl_height = 100, 120
+## Load player image and re-size it 
+PLImg = pygame.transform.scale(pygame.image.load("img/Player.png"), (pl_with, pl_height))
+## Mass of the red rectangle
+pl_mass = 1.5  # Smaller mass
+
+# Buoyancy parameters:
+## Initial position of the water level
+water_level = res_y * 2 // 3  
+## Density of water (higher value for denser water)
+water_density = 4.5  
+gravity = 9.8
+buoyant_force = 5
+
+# Draws everything on the scene
 def draw():
     # Draw Bckgroung image
-    screen.blit(screenUpdate, (0, 0))
+    screen.blit(BGImg, (0, 0))
+    # Draw player in a determined location
+    screen.blit(PLImg, (200, res_y - pl_height))
+
     # Update screen
     pygame.display.update()
+
+
 
 def main():
     # Initialize pygame, with the default parameters
     pygame.init()
+
+    player = pygame.Rect(200, res_y - pl_height, pl_with, pl_height)
 
     # Game loop, runs forever
     while (True):
